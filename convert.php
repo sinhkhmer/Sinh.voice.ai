19398a5c99f71c981558f158766be04c5c9640b9
Sinh <107097399+sinhkhmer@users.noreply.github.com>
2023-11-24 21:20:35 +0700

    Create convert.php

---
+++ convert.php
@@ -0,0 +1,29 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Check if the file is uploaded successfully
+    if (isset($_FILES['videoFile']) && $_FILES['videoFile']['error'] === UPLOAD_ERR_OK) {
+        $videoFileName = $_FILES['videoFile']['name'];
+        $videoTmpName = $_FILES['videoFile']['tmp_name'];
+
+        // Create a unique filename for the WAV file
+        $wavFileName = uniqid('converted_', true) . '.wav';
+        $wavFilePath = 'converted/' . $wavFileName;
+
+        // Use FFmpeg to convert the video to WAV
+        $command = "ffmpeg -i $videoTmpName -f wav $wavFilePath";
+        shell_exec($command);
+
+        // Check if the conversion was successful
+        if (file_exists($wavFilePath)) {
+            // Provide a download link to the user
+            echo json_encode(['success' => true, 'wavLink' => $wavFilePath]);
+        } else {
+            echo json_encode(['success' => false]);
+        }
+    } else {
+        echo json_encode(['success' => false]);
+    }
+} else {
+    echo json_encode(['success' => false]);
+}
+?>
