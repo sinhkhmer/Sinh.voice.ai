c1db561f5934af8e11516fe082b8c48f57913cce
Sinh <107097399+sinhkhmer@users.noreply.github.com>
2023-11-24 21:33:40 +0700

    Update convert.php

--- convert.php
+++ convert.php
@@ -9,21 +9,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $wavFileName = uniqid('converted_', true) . '.wav';
         $wavFilePath = 'converted/' . $wavFileName;
 
+        // Debugging: Log intermediate steps
+        error_log("Video file name: $videoFileName", 0);
+        error_log("Video tmp name: $videoTmpName", 0);
+        error_log("WAV file name: $wavFileName", 0);
+        error_log("WAV file path: $wavFilePath", 0);
+
         // Use FFmpeg to convert the video to WAV
         $command = "ffmpeg -i $videoTmpName -f wav $wavFilePath";
         shell_exec($command);
 
-        // Check if the conversion was successful
+        // Debugging: Log result of the conversion
         if (file_exists($wavFilePath)) {
-            // Provide a download link to the user
+            error_log("Conversion successful. WAV file exists.", 0);
             echo json_encode(['success' => true, 'wavLink' => $wavFilePath]);
         } else {
+            error_log("Conversion failed. WAV file does not exist.", 0);
             echo json_encode(['success' => false]);
         }
     } else {
+        error_log("File upload failed.", 0);
         echo json_encode(['success' => false]);
     }
 } else {
+    error_log("Invalid request method.", 0);
     echo json_encode(['success' => false]);
 }
 ?>
